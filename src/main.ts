24b9d9e7c2ed76395dc3af07dc24ac16bd4da4ee
marechal-dev <pietro.developer@gmail.com>
2025-03-24 19:22:36 -0300

    feat: add prime vue

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +10,14 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(PrimeVue, {
+	options: {
+		cssLayer: {
+			name: 'primevue',
+			order: 'theme, base, primevue'
+		}
+	}
+})
 app.use(router)
 
 app.mount('#app')
